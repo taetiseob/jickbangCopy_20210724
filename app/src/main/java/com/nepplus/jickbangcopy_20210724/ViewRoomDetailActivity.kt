34f4f3e4a109de861325f5a25bc99190663292b4
김태섭 <kyoer234@gmail.com>
2021-07-24 12:23:30 +0900

    넘겨준 데이터 이용, 방 정보 표현

--- app/src/main/java/com/nepplus/jickbangcopy_20210724/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/nepplus/jickbangcopy_20210724/ViewRoomDetailActivity.kt
@@ -3,6 +3,7 @@ package com.nepplus.jickbangcopy_20210724
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nepplus.jickbangcopy_20210724.datas.RoomData
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +11,10 @@ class ViewRoomDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view_room_detail)
         val roomData = intent.getSerializableExtra("roomInfo") as RoomData
 
+        priceTxt.text = roomData.getFormattedPrice()
+        descTxt.text = roomData.desc
+        addressTxt.text = roomData.address
+        floorTxt.text = roomData.getFloor()
 
 
     }
